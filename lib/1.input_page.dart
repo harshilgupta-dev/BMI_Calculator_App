d02bc8c243f005cb576c93018d075e975cdb7716
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-14 22:04:58 +0530

    import font_awesome_flutter file

--- lib/1.input_page.dart
+++ lib/1.input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const cardColorOnChange = 0xFF1D1E33;
