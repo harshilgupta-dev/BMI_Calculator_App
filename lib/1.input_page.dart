c4501264307d80a61443c38fe9fb624f181d292f
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-14 22:33:30 +0530

    add IconContent constructor

--- lib/1.input_page.dart
+++ lib/1.input_page.dart
@@ -90,20 +90,26 @@ class ReusableCard extends StatelessWidget {
 }
 
 class IconContent extends StatelessWidget {
+
+  final String label;
+  final IconData icon;
+
+  IconContent({this.icon, this.label})
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Icon(
-          FontAwesomeIcons.mars,
+          icon,
           size: 80,
         ),
         SizedBox(
           height: 10.0,
         ),
         Text(
-          'Male',
+          label,
           style: TextStyle(
             fontSize: 18.0,
             color: Color(0xFF8D8E98),
