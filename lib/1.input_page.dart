d7ab939abe7bbc0413f1d8d1069e5557fb422170
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-16 01:13:13 +0530

    add icon property and RoundIconButton Constructor

--- lib/1.input_page.dart
+++ lib/1.input_page.dart
@@ -129,11 +129,17 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ResuableCard(
                       cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
                             'HEIGHT',
                             style: kLabelTextStyle,
                           ),
+                          Text(
+                            weight.toString(),
+                            style: kNumberTextStyle,
+                          ),
+                          RoundIconButton()
                         ],
                       ),
                       colour: kActiveCardColour),
@@ -155,12 +161,18 @@ class _InputPageState extends State<InputPage> {
 }
 
 class RoundIconButton extends StatelessWidget {
-  const RoundIconButton({Key key}) : super(key: key);
+  RoundIconButton({this.icon});
+
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
       shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+      constraints: BoxConstraints.tightFor(width: 50.0, height: 50.0),
+      child: Icon(icon),
+      onPressed: () {},
     );
   }
 }
