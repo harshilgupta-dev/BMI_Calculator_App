a7142267d541e9485af7a2ca6e0e2810a39b57a8
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-15 21:27:00 +0530

    shift 'cm' to baseline

--- lib/1.input_page.dart
+++ lib/1.input_page.dart
@@ -79,6 +79,8 @@ class _InputPageState extends State<InputPage> {
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
                         children: [
                           Text(
                             '180',
