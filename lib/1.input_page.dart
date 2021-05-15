d3103c0c6fdf25a89f258dae9bb3ab07735d3674
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-15 17:54:58 +0530

    add all constants to constants.dart file

--- lib/1.input_page.dart
+++ lib/1.input_page.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const inactiveCardColour = Color(0xFF111328);
-const bottomContainerColour = Color(0xFFEB1555);
 
 enum Gender { male, female }
 
