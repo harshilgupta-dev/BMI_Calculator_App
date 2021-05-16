f9bbffc10dac8bf8d13f3de95f470f65fa4ef5df
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-16 21:37:39 +0530

    create calculator brain class and all methods

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain(this.weight, this.height);
+
+  final int weight;
+  final int height;
+  double _bmi;
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi >= 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher weight than normal body weight.Try to excerise more.';
+    } else if (_bmi >= 18.5) {
+      return 'You have a normal weight.Good job,Keep it up ';
+    } else {
+      return 'You have a lower weight than a normal body weight. You can eat bit more.';
+    }
+  }
+}
